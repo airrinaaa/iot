51c6ddc5d2ea4f34271c05a0f1f6242964cca5ee
Iryna Rychok <i.rychok@ukma.edu.ua>
2026-02-24 15:22:55 +0200

    processing all sensors by their types

--- CollectAll.py
+++ CollectAll.py
@@ -66,22 +66,61 @@ class CollectAll(WindowFunction):
                 "delta": sorted_data[-1]["value"] - sorted_data[0]["value"],
             }
         elif type == Type.ANALOG:
-            sorted_data = sorted(all_data, key=lambda x: x["value"])
+            all_values = [event["value"] for event in all_data]
+            all_values = sorted(all_values)
+            count = len(all_values)
+            sum_value = sum(all_values)
+
             yield {
                 "datastream_id": key,
                 "window": f"{window_start} - {window_end}",
                 "count": len(all_data),
                 "metric": all_data[0]['metric'],
-                "min value": sorted_data[0]["value"],
-                "max value": sorted_data[-1]["value"],
-                "average": sorted_data,
+                "min value": all_values[0],
+                "max value": all_values[-1],
+                "average": sum_value/count,
             }
-        else:
+        elif type == Type.STATE:
+            sorted_data = sorted(all_data, key=lambda x: x["event_time"])
+            all_values = [event["value"] for event in sorted_data]
+            count_on = 0
+            count_off = 0
+            for val in all_values:
+                if val == "ON":
+                    count_on += 1
+                elif val == "OFF":
+                    count_off += 1
+
             yield {
                 "datastream_id": key,
                 "window": f"{window_start} - {window_end}",
                 "count": len(all_data),
                 "metric": all_data[0]['metric'],
-                "data": all_data,
+                "distinct counts": len(set(all_values)),
+                "on-count": count_on,
+                "off-count": count_off,
+                "last state": sorted_data[-1]["value"],
+            }
+        elif type == Type.ALARM:
+            sorted_data = sorted(all_data, key=lambda x: x["event_time"])
+            all_values = [event["value"] for event in sorted_data]
+            count_true = 0
+            count_false = 0
+            for val in all_values:
+                if val:
+                    count_true += 1
+                elif not val:
+                    count_false += 1
+            yield {
+                "datastream_id": key,
+                "window": f"{window_start} - {window_end}",
+                "count": len(all_data),
+                "metric": all_data[0]['metric'],
+                "distinct counts": len(set(all_values)),
+                "triggered": any(all_values),
+                "true-count": count_true,
+                "false-count": count_false,
+                "true_ratio": count_true / len(all_values),
+                "last value": sorted_data[-1]["value"],
             }
 
